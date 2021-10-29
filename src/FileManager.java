2fc13d8a6ec417a66d481a09b7dec86f81ecdb72
Mr.Strelan <daniil..strelan@gmail.com>
2021-10-29 08:35:14 +0200

    mmm

--- src/FileManager.java
+++ src/FileManager.java
@@ -1,23 +1,46 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class FileManager
-{
+public class FileManager {
+    //Creating files
+    File usernames = new File("Accounts.txt");
+    private Scanner reads;
 
-        File usernames = new File("Accounts.txt");
-        File passwords = new File("Passwords.txt");
-        FileOutputStream addUsernames = new FileOutputStream(usernames, false);
-        FileOutputStream addPasswords = new FileOutputStream(passwords, false);
-        PrintStream inputUser = new PrintStream(addUsernames);
-        PrintStream inputPass = new PrintStream(addPasswords);
-        Scanner reads = new Scanner("Accounts.txt");
-    {
-        System.out.println("File not found");
+    public void addUser(String username) {
+        try {
+            //Deciding how we gonna write in the files
+            FileOutputStream addUsernames = new FileOutputStream(usernames, true);
+            PrintStream inputUser = new PrintStream(addUsernames);
+            inputUser.println(username);
+            inputUser.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Adder didn't found file");
+        }
     }
 
-    public void add(String username)
+    public String readNextLine () {
+        String line = "";
+        try {
+            reads = new Scanner(usernames);
+            line = reads.nextLine();
+        }catch (FileNotFoundException e) {
+            System.out.println("Reader didn't got file");
+        }
+        return line;
+    }
+
+    public boolean checkLine()
     {
-        inputUser.println(username);
+        boolean line = false;
+        try {
+            reads = new Scanner(usernames);
+            line = reads.hasNextLine();
+            String a = reads.nextLine();
+        }catch (FileNotFoundException e)
+        {
+            System.out.println("Didn't found file while checking line");
+        }
+        return line;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,60 @@
 import java.io.*;
 import java.util.Scanner;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-            FileManager manag = new FileManager();
-            Account test = new Account();
-            boolean creatingAccount = false;
-            while (creatingAccount == false) {
-                System.out.println("1. Log in");
-                System.out.println("2. Create account");
-                Scanner inputSc = new Scanner(System.in);
-                int input;
-                input = inputSc.nextInt();
-                if (input == 1) {
-                    creatingAccount = true;
-                    boolean creatingUsername = false;
-                    while(creatingUsername == false) {
-                        System.out.println("Enter username");
-                        String enteredUsername = inputSc.nextLine();
-                            while (manag.reads.hasNextLine()) {
-                                if (enteredUsername == manag.reads.nextLine()) {
-                                    System.out.println("This username is already taken, try another one");
-                                } else {
-                                    creatingUsername = true;
-                                    manag.add(enteredUsername);
-                                }
+public class Main {
+    public static void main(String[] args) {
+        FileManager funk1 = new FileManager();
+        boolean creatingAccount = true;
+        while (creatingAccount == true) {
+            System.out.println("1. Log in");
+            System.out.println("2. Create account");
+            Scanner menuSc = new Scanner(System.in);
+            int input = menuSc.nextInt();
+            if (input == 1) {
+                System.out.println("Enter username");
+
+            } else if (input == 2) {
+
+                System.out.println("Create username");
+                Scanner usernameSc = new Scanner(System.in);
+                //Puts users input into variable "enterUsername"
+                String enteredUsername = "";
+                enteredUsername = usernameSc.nextLine();
+
+                boolean creatingUsername = true;
+                while (creatingUsername == true) {
+                    //Checks if Username list is not empty
+                    if (funk1.checkLine() == true) {
+                        //Checks for matches of Usernames
+                        boolean matchesUsername = false;
+                        while (funk1.checkLine()==true) {
+                            String username = "";
+                            username = funk1.readNextLine();
+                            if (enteredUsername.equals(username)){
+                                matchesUsername = true;
                             }
-
+                        }
+                        //If match is not found adds username
+                        if (matchesUsername == false) {
+                            funk1.addUser(enteredUsername);
+                            creatingUsername = false;
+                            creatingAccount = false;
+                            //If match is found asks to repeat
+                        } else {
+                            System.out.println("This username is already taken, try another one");
+                        }
+                        //Adds user is Username list is empty
+                    } else {
+                        funk1.addUser(enteredUsername);
+                        creatingUsername = false;
+                        creatingAccount = false;
                     }
-
-                } else if (input == 2) {
-                    creatingAccount = true;
-                    System.out.println("Create username");
-
-                } else {
-                    System.out.println("This option is not on menu, try again");
                 }
+
+            } else {
+                System.out.println("This option is not on menu, try again");
             }
+        }
 
     }
 }
