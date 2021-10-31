00ee366c73943a2408eb0a65cac57735a98b62c8
Mr.Strelan <daniil..strelan@gmail.com>
2021-10-31 13:25:31 +0100

    1.56

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Load Movie Database (MovieDB is an Arraylist of Movie Objects)
-
+        Menu.menuLog();
         ArrayList<Movie> movDB = new ArrayList<>();
         System.out.println(MovieDB.ReadDB().get(0).toString());
         //MovieDB.deleteMovieFromDB();//deletes movie from DB asking for movie ID
