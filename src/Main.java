dea0427c8afe9dbb5d0f5786253f9e5f86c7a4c3
filippo <filippo-ceradini>
2021-11-01 09:07:38 +0100

    Polished find bytitlef

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.io.*;
 import java.sql.Struct;
 import java.util.ArrayList;
 import java.util.Scanner;
-import kotlin.Pair;
 
 public class Main {
     public static void main(String[] args) {
 
         //Load Movie Database (MovieDB is an Arraylist of Movie Objects)
         Menu.print(MovieDB.ReadDB().toString());
+        //Menu.menuAdmin();
         Menu.menuLog();
 
     }
